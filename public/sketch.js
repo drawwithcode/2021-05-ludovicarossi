16885815e2b4aa0481ab3eebb2f8972851510f23
ludovicarossi <ludovica4.rossi@mail.polimi.it>
2021-12-07 12:42:34 +0100

    Added socket.

--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,17 @@
+//CLIENT-SIDE
+
+//activating the socket library
+let clientSocket = io();
+
+//running newConnection function
+clientSocket.on("connect", newConnection);
+
+//defining the newConnection function
+function newConnection() {
+  //printing the connections' ids
+  console.log(clientSocket.id);
+}
+
 function setup() {
   createCanvas(400, 400);
 }
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//SERVER-SIDE
+
 //testing on terminal
 console.log("up and running");
 
@@ -18,3 +20,19 @@ console.log("Server is running on http://localhost:" + port);
 
 //using public folder to send (static) files
 app.use(express.static("public"));
+
+//introducing socket library
+let serverSocket = require("socket.io");
+
+//creating object for incoming&outcoming messages
+let io = serverSocket(server);
+
+//attaching code that will be executed when someone connects to the server
+io.on("connection", newConnection);
+
+//defining the newConnection function
+//the object newSocket contains all the informations the socket library will pass
+function newConnection(newSocket) {
+  //printing the unique id of every client connection
+  console.log(newSocket.id);
+}
