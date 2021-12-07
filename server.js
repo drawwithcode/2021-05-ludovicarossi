4b2ab4f01dce702714d1724f7364155adfdb8d83
ludovicarossi <ludovica4.rossi@mail.polimi.it>
2021-12-07 11:23:05 +0100

    Added sketch.

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+//testing on terminal
 console.log("up and running");
+
+//loading express library into a variable
+let express = require("express");
+
+//running express library inside the application
+let app = express();
+
+//defining a port
+let port = 3000;
+
+//activating the application; variable containing the running instances of the server
+let server = app.listen(port);
+
+//checking
+console.log("Server is running on http://localhost:" + port);
+
+//using public folder to send (static) files
+app.use(express.static("public"));
